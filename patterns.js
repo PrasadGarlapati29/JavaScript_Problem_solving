f98635a1963c2d7d5b44c583e816d996c49496e2
PrasadGarlapati29 <pgarlapati4@gmail.com>
2025-09-24 10:12:23 +0530

    Update patterns.js

--- patterns.js
+++ patterns.js
@@ -1,11 +1,46 @@
-for(let i=0;i<8;i++){
+//Right angled Trinagle
+
+// for(let i=0;i<8;i++){
   
-  stars=""
-  for(let k=0;k<8-i;k++){
-    stars+="  "
-  }
-  for(let j=0;j<1+i;j++){
-    stars+=" *"
-  }
-  console.log(stars)
-}
+//   stars=""
+//   for(let k=0;k<8-i;k++){
+//     stars+="  "
+//   }
+//   for(let j=0;j<1+i;j++){
+//     stars+=" *"
+//   }
+//   console.log(stars)
+// }
+
+
+
+//Hollow sqaure
+// let n=12
+// let count=1
+// for(let i=0;i<n;i++){
+//   stars=""
+//   for(let j=0;j<n;j++){
+//     if(i==0 || i==n-1 || j==0 || j==n-1){
+      
+//        if(count<10){
+//          stars+=count+"  "
+//        }
+//        else{
+         
+//       stars+=count+" "
+//        }
+      
+//       count+=1
+//     }else{
+//       if(count>=10){
+//         stars+="   "
+//       }else{
+//         stars+=" "
+//       }
+      
+//     }
+//   }
+//   console.log(stars)
+// }
+
+
